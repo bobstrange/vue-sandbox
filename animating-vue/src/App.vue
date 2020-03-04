de8dd678268af94841ebcdf8a0d9b45478519e6b
bob1983 <bob.1983.g@gmail.com>
2020-03-05 00:00:01 +0900

    init and styling a bit

--- animating-vue/src/App.vue
+++ animating-vue/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="app">
-    Hi there!
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
@@ -11,5 +15,70 @@ export default {
 </script>
 
 <style>
+html {
+  font-size: 62.5%;
+}
+:root {
+  --color-primary: #16c0b0;
+  --color-primary-light: #84cf6a;
 
+  --color-grey-light: #666;
+  --color-grey-dark: #333;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  box-sizing: border-box;
+}
+
+button {
+  width: 8rem;
+  height: 4rem;
+  border-radius: 5px;
+  background: linear-gradient(to right, var(--color-primary), var(--color-primary-light));
+  font-size: 2rem;
+  color: white;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  margin-top: 2rem;
+}
+
+#nav {
+  padding: 2rem;
+  background-image: linear-gradient(
+    to right,
+    rgba(64, 64, 64, .4),
+    rgba(32, 32, 32, .4)
+  );
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#nav a {
+  font-size: 2rem;
+  font-weight: bold;
+  color: #2c3e50;
+  display: inline-box;
+  padding: .5rem, 2rem;
+  width: 10%;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.modal {
+  width: 250px;
+  height: 200px;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 20px;
+  border-radius: 2%;
+  background-color: #e0e0e0;
+}
 </style>
