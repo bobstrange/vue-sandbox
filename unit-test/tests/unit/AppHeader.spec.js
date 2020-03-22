7c5006b9acb11e5b04a0540224c2152aa762a452
bob1983 <bob.1983.g@gmail.com>
2020-03-22 22:27:08 +0900

    pass tests

--- unit-test/tests/unit/AppHeader.spec.js
+++ unit-test/tests/unit/AppHeader.spec.js
@@ -6,8 +6,11 @@ describe('AppHeader', () => {
     const wrapper = mount(AppHeader)
     expect(wrapper.find('button').isVisible()).toBeFalsy()
   })
-  test('If user is logged in, show logout button', () => {
+  test('If user is logged in, show logout button', async () => {
     const wrapper = mount(AppHeader)
+    wrapper.setData({ loggedIn: true })
+
+    await wrapper.vm.$nextTick() // Need to wait dom updates
     expect(wrapper.find('button').isVisible()).toBeTruthy()
   })
 })
