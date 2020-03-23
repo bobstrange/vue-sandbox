65785c4b1d2094caed511b61b53ac27ca0a33879
bob1983 <bob.1983.g@gmail.com>
2020-03-23 11:38:23 +0900

    tmp

--- vuetify-sandbox/src/App.vue
+++ vuetify-sandbox/src/App.vue
@@ -1,30 +1,74 @@
 <template>
   <v-app>
-    <v-card width=400 class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            label="username"
-            prepend-icon="mdi-account-circle"
-          />
-          <v-text-field
-            :type="showPassword ? 'text' : 'password'"
-            label="password"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>vuetify sandbox</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+
+    </v-app-bar>
+    <v-content>
+      <v-card width=400 class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="username"
+              prepend-icon="mdi-account-circle"
+            />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+
+    <v-footer
+      color="primary lighten-1"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -37,7 +81,11 @@ export default {
   },
 
   data: () => ({
-    showPassword: false
+    showPassword: false,
+    links: [
+      'Home',
+      'Login'
+    ]
   }),
 };
 </script>
