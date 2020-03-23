cf81c4524681183a78ea46b3cb64bd961ca80504
bob1983 <bob.1983.g@gmail.com>
2020-03-23 20:25:30 +0900

    router and error

--- nuxt-sandbox/components/NavBar.vue
+++ nuxt-sandbox/components/NavBar.vue
@@ -5,7 +5,7 @@
     </nuxt-link>
     <nav>
       <nuxt-link to="/" class="brand">List</nuxt-link>
-      <nuxt-link to="/create" class="brand">Create</nuxt-link>
+      <nuxt-link to="/events/create" class="brand">Create</nuxt-link>
     </nav>
   </div>
 </template>
---
+++ nuxt-sandbox/pages/events/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Event: #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  },
+  head() {
+    return {
+      title: `Event ${this.id}`
+    }
+  }
+}
+</script>
