fa8447d951889267233434fd57ef5344d85bbe49
bob1983 <bob.1983.g@gmail.com>
2020-03-27 15:32:43 +0900

    Added dashboard

--- vuetify-sandbox/src/router/index.js
+++ vuetify-sandbox/src/router/index.js
@@ -6,25 +6,30 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../views/Dashboard.vue")
   }
-
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
@@ -36,5 +41,6 @@ export default router
 
 export const navigationLinks = [
   { label: 'Home', routeName: 'Home' },
-  { label: 'Login', routeName: 'Login' }
+  { label: 'Login', routeName: 'Login' },
+  { label: 'Dashboard', routeName: 'Dashboard' }
 ]
