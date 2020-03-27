51f3ebc9290fe80f4573d9022093f2b68d3cda19
bob1983 <bob.1983.g@gmail.com>
2020-03-27 16:51:15 +0900

    base board

--- trello-clone/src/views/Board.vue
+++ trello-clone/src/views/Board.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="board">
-    <div class="columns">
-      <div class="column" v-for="(column, index) in board.columns" :key="index">
-        {{ column }}
-      </div>
-    </div>
+    <v-container>
+      <v-row class="board__titles">
+        <v-col
+          class="column"
+          v-for="(column, index) in board.columns"
+          :key="index"
+        >
+          <v-card
+            class="mx-auto column__base"
+            max-width="344"
+            outlined
+            style="background-color: #D4DEE5;"
+          >
+            <v-card-title class="column__title">
+              {{ column.name }}
+            </v-card-title>
+
+            <v-card
+              class="mx-auto column__task"
+              max-width="344"
+              outlined
+              v-for="(task, i) in column.tasks"
+              :key="i"
+            >
+              <v-card-title>
+                {{ task.name }}
+              </v-card-title>
+              <v-card-text v-if="task.description">
+                {{ task.description }}
+              </v-card-text>
+            </v-card>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -22,18 +52,25 @@ export default {
 
 <style scoped>
 .board {
-  background-color: #02a295;
+  background-color: #57bf7a;
+  min-height: 100vh;
 }
 
-.columns {
-  display: flex;
-  justify-content: flex-start;
-  background-color: white;
+.column__base {
+  padding: 0.5rem;
+  background-color: #D4DEE5;
 }
 
-.column {
-  width: 20%;
-  margin: 0 auto;
+.column__task {
+  background-color: #D4DEE5;
 }
 
+.column__title {
+  font-size: 1.2rem;
+  padding: 0.2rem 0.2rem;
+}
+
+.column__task:not(:last-child) {
+  margin-bottom: 0.5rem;
+}
 </style>
