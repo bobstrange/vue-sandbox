25e81be3593cb4e4950c6d543827ba15e26f0695
bob1983 <bob.1983.g@gmail.com>
2020-04-03 23:56:38 +0900

    user getter spec

--- vuex-smart-module-example/src/store/__tests__/user.store.spec.ts
+++ vuex-smart-module-example/src/store/__tests__/user.store.spec.ts
@@ -3,8 +3,10 @@ import { UserGetters, UserActions } from "@/store/user.store"
 import { User } from "@/models/User"
 
 describe("UserStore", () => {
-  test("returns a given id's user", () => {
-    const getters = inject(UserGetters, {
+  let getters: UserGetters
+
+  beforeEach(() => {
+    getters = inject(UserGetters, {
       state: {
         users: [
           { id: 1, name: "foo" },
@@ -12,6 +14,13 @@ describe("UserStore", () => {
         ] as User[]
       }
     })
+  })
+
+  test("returns a given id's user", () => {
     expect(getters.getUser(2)).toEqual({ id: 2, name: "bar" })
+  })
+
+  test("returns undefined if given id user does not exist", () => {
+    expect(getters.getUser(3)).toBeUndefined()
   })
 })
