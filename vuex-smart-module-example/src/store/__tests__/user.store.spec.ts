603b3f9e32fa510ef02a1658d34aede822fcc187
bob1983 <bob.1983.g@gmail.com>
2020-04-03 23:57:49 +0900

    Added failed test. Need to mock UserAPI

--- vuex-smart-module-example/src/store/__tests__/user.store.spec.ts
+++ vuex-smart-module-example/src/store/__tests__/user.store.spec.ts
@@ -2,25 +2,39 @@ import { inject } from "vuex-smart-module"
 import { UserGetters, UserActions } from "@/store/user.store"
 import { User } from "@/models/User"
 
-describe("UserStore", () => {
-  let getters: UserGetters
+describe("UserGetters", () => {
+  describe('getUser', () => {
+    let getters: UserGetters
 
-  beforeEach(() => {
-    getters = inject(UserGetters, {
-      state: {
-        users: [
-          { id: 1, name: "foo" },
-          { id: 2, name: "bar" }
-        ] as User[]
-      }
+    beforeEach(() => {
+      getters = inject(UserGetters, {
+        state: {
+          users: [
+            { id: 1, name: "foo" },
+            { id: 2, name: "bar" }
+          ] as User[]
+        }
+      })
+    })
+
+    test("returns a given id's user", () => {
+      expect(getters.getUser(2)).toEqual({ id: 2, name: "bar" })
+    })
+
+    test("returns undefined if given id user does not exist", () => {
+      expect(getters.getUser(3)).toBeUndefined()
     })
   })
+})
 
-  test("returns a given id's user", () => {
-    expect(getters.getUser(2)).toEqual({ id: 2, name: "bar" })
-  })
+describe('UserActions', () => {
+  describe('fetchUsers', () => {
+    test('', async () => {
+      const commit = jest.fn()
+      const actions = inject(UserActions, { commit })
+      await actions.fetchUsers()
 
-  test("returns undefined if given id user does not exist", () => {
-    expect(getters.getUser(3)).toBeUndefined()
+      expect(commit).toHaveBeenCalledWith('UPDATE_USERS', [])
+    })
   })
 })
