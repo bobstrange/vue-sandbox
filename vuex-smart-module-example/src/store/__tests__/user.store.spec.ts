93aee6691aca90a57fce597fcb2a993e5b2bf3ef
bob1983 <bob.1983.g@gmail.com>
2020-04-04 22:44:09 +0900

    Stub fetchUsers

--- vuex-smart-module-example/src/store/__tests__/user.store.spec.ts
+++ vuex-smart-module-example/src/store/__tests__/user.store.spec.ts
@@ -1,6 +1,7 @@
 import { inject } from "vuex-smart-module"
 import { UserGetters, UserActions } from "@/store/user.store"
 import { User } from "@/models/User"
+import { UserAPI } from '@/api/user'
 
 describe("UserGetters", () => {
   describe('getUser', () => {
@@ -27,14 +28,27 @@ describe("UserGetters", () => {
   })
 })
 
+jest.mock('@/api/user')
+
 describe('UserActions', () => {
   describe('fetchUsers', () => {
     test('', async () => {
+      (UserAPI as any).mockImplementation(() => {
+        return {
+          fetchUsers: jest.fn().mockResolvedValue([
+            { id: 1, name: 'foo' },
+            { id: 2, name: 'bar' }
+          ])
+        }
+      })
       const commit = jest.fn()
       const actions = inject(UserActions, { commit })
       await actions.fetchUsers()
 
-      expect(commit).toHaveBeenCalledWith('UPDATE_USERS', [])
+      expect(commit).toHaveBeenCalledWith('UPDATE_USERS', [
+        { id: 1, name: 'foo' },
+        { id: 2, name: 'bar' }
+      ])
     })
   })
 })
