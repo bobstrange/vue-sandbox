038e3e57c8cfb2082924313cdb1094910f6fd789
bob1983 <bob.1983.g@gmail.com>
2020-04-07 22:04:26 +0900

    Added test and beforeEach

--- unit-test/tests/unit/ApiCaller.spec.js
+++ unit-test/tests/unit/ApiCaller.spec.js
@@ -6,6 +6,10 @@ import flushPromises from 'flush-promises'
 jest.mock('@/services/api')
 
 describe('ApiCaller', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('Calls getMessage and displays message', async () => {
     getMessage.mockResolvedValueOnce({ text: 'mock message text' })
     const wrapper = mount(ApiCaller)
@@ -15,5 +19,16 @@ describe('ApiCaller', () => {
     const paragraph = wrapper.find('[data-testid="message"]')
     const message = paragraph.element.textContent
     expect(message).toEqual('mock message text')
+  })
+
+  test('Calls getMessage and displays error if api call fails', async () => {
+    getMessage.mockRejectedValueOnce(new Error())
+    const wrapper = mount(ApiCaller)
+    await flushPromises()
+
+    expect(getMessage).toHaveBeenCalledTimes(1)
+    const errorParagraph = wrapper.find('[data-testid="message-error"]')
+    const message = errorParagraph.element.textContent
+    expect(message).toEqual('Something went wrong')
   })
 })
