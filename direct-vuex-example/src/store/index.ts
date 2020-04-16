dcc05f4332cec5f2b6870ab280d016bb352dc25b
bob1983 <bob.1983.g@gmail.com>
2020-04-16 23:17:51 +0900

    Updated store

--- direct-vuex-example/src/store/index.ts
+++ direct-vuex-example/src/store/index.ts
@@ -1,8 +1,32 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { ActionContext } from "vuex";
+import { User } from '@/models/User'
+import { fetchUsers } from '@/services/user'
 
 Vue.use(Vuex);
 
+type UserState = {
+  users: User[]
+}
+
+const user = {
+  namespaced: true,
+  state: {
+    users: []
+  } as UserState,
+  getters: {},
+  mutations: {
+    SET_USERS(state: UserState, users: User[]) {
+      state.users = users
+    }
+  },
+  actions: {
+    async getUsers(ctx: ActionContext<UserState, {}> , payload: User[]) {
+      const users = await fetchUsers()
+      ctx.commit('SET_USERS', users)
+    }
+  }
+}
 export default new Vuex.Store({
   state: {},
   mutations: {},
