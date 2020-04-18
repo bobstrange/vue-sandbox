e20ecda6ceafd95f73341f7b26d75de3d8158acb
bob1983 <bob.1983.g@gmail.com>
2020-04-18 23:40:18 +0900

    tmp

--- vue-composition-api-with-ts/src/App.vue
+++ vue-composition-api-with-ts/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <h1>Hello</h1>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
-  name: 'App',
-  components: {}
-})
+  name: "App",
+  components: {
+    HelloWorld
+  }
+});
 </script>
 
 <style lang="scss">
