2af8f24ec123c5561a6392490e1eaf2ac98017aa
bob1983 <bob.1983.g@gmail.com>
2020-04-18 23:26:16 +0900

    init

---
+++ vue-composition-api-with-ts/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: {}
+})
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
