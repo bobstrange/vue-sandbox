28664093f112619e546f8ddde846db47baf12b79
bob1983 <bob.1983.g@gmail.com>
2020-04-26 23:47:36 +0900

    Implement messy component

--- renderless-component/src/views/Home.vue
+++ renderless-component/src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <BaseButton
+      text="Submit"
+      :isLoading="loading"
+      iconRightName="paper-plane"
+      iconLeftName="right-arrow"
+      :isLoadingLeft="loadingLeft"
+      :isLoadingRight="loadingRight"
+    />
+  </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from '@vue/composition-api'
+import BaseButton from '@/components/BaseButton'
+export default defineComponent({
   name: 'Home',
-  components: {}
-}
+  components: { BaseButton },
+  setup() {
+    const isLoading = ref(false)
+    const isLoadingLeft = ref(false)
+    const isLoadingRight = ref(false)
+    return { isLoading, isLoadingLeft, isLoadingRight }
+  }
+})
 </script>
