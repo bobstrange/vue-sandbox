f34fa8a8f81d13a08195476e93de609ce9cfbf5b
bob1983 <bob.1983.g@gmail.com>
2020-05-06 23:21:25 +0900

    Added MessageDisplay component

---
+++ unit-test/src/components/MessageDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <p v-if="error" data-testid="message-error">{{ error }}</p>
+  <p v-else data-testid="messege">{{ message.text }}</p>
+</template>
+
+<script>
+import { getMessage } from '../services/api'
+
+export default {
+  data() {
+    return {
+      message: {},
+      error: null
+    }
+  },
+  async created() {
+    try {
+      this.message = await this.getMessage
+    } catch (e) {
+      this.error = 'Something went wrong'
+    }
+  }
+}
+</script>
+
+<style></style>
