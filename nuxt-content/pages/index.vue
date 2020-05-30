e416bcf9df675359554a32b45affad2a33cd2fa5
bob1983 <bob.1983.g@gmail.com>
2020-05-30 12:14:04 +0900

    add home content

--- nuxt-content/pages/index.vue
+++ nuxt-content/pages/index.vue
@@ -1,37 +1,24 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxt-content
-      </h1>
-      <h2 class="subtitle">
-        My luminous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <article>
+        <nuxt-content :document="doc" />
+      </article>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
 
 export default Vue.extend({
-  components: {
-    Logo
-  }
+  async asyncData({ $content }) {
+    const doc = await $content('home').fetch()
+    return {
+      doc
+    }
+  },
+  components: {}
 })
 </script>
 
