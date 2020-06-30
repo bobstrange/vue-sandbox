06247ffb088cc85e9cb29ec76481b6c6625e7c2f
bob1983 <bob.1983.g@gmail.com>
2020-06-30 23:09:05 +0900

    tmp

--- from-vue2-to-vue3/src/App.vue
+++ from-vue2-to-vue3/src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <h1>From vue2 to vue3</h1>
+  <input :type="type" v-model="email" />
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: "App",
+  data() {
+    return {
+      email: "",
+      type: "email",
+    };
+  },
 };
 </script>
