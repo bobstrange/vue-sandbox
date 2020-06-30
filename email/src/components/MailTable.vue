c683b82b0b93bfd94d5d2ca256115a97a3d087b5
bob1983 <bob.1983.g@gmail.com>
2020-07-01 00:17:42 +0900

    Show mail detail

--- email/src/components/MailTable.vue
+++ email/src/components/MailTable.vue
@@ -5,7 +5,7 @@
         v-for="email in unarchivedEmails"
         :key="id"
         :class="['clickable', email.read ? 'read' : '']"
-        @click="onClickReadEmail(email)"
+        @click="onClickEmail(email)"
       >
         <td><input type="checkbox" /></td>
         <td>{{ email.from }}</td>
@@ -21,12 +21,15 @@
       </tr>
     </tbody>
   </table>
+  <MailView v-if="selectedEmail" :email="selectedEmail" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { format } from 'date-fns'
 import axios from 'axios'
+
+import MailView from './MailView.vue'
 
 const updateEmail = async email => {
   await axios.put(`http://localhost:3001/emails/${email.id}`, email)
@@ -36,10 +39,15 @@ export default defineComponent({
   name: 'MailTable',
   async setup() {
     const { data: emails } = await axios.get('http://localhost:3001/emails')
-    const onClickReadEmail = email => {
+
+    const selectedEmail = ref(null)
+
+    const onClickEmail = email => {
       email.read = true
       updateEmail(email)
+      selectedEmail.value = email
     }
+
     const onClickArchiveEmail = email => {
       email.archived = true
       updateEmail(email)
@@ -48,7 +56,8 @@ export default defineComponent({
     return {
       format,
       emails,
-      onClickReadEmail,
+      selectedEmail,
+      onClickEmail,
       onClickArchiveEmail
     }
   },
@@ -61,6 +70,9 @@ export default defineComponent({
     unarchivedEmails() {
       return this.sortedEmails.filter(email => !email.archived)
     }
+  },
+  components: {
+    MailView
   }
 })
 </script>
---
+++ email/src/components/MailView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="email-display">
+    <h2 class="mb-0">
+      Subject: <strong>{{ email.subject }}</strong>
+    </h2>
+    <div>
+      <em>From {{ email.from }} on {{ email.sentAt }}</em>
+    </div>
+    <div>{{ email.body }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup(props) {},
+  props: {
+    email: {
+      type: Object,
+      required: true
+    }
+  }
+})
+</script>
+
+<style></style>
