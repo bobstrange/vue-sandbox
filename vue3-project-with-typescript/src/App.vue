5b4934cb7937628bc7990fd2164d8441b2c355f3
bob1983 <bob.1983.g@gmail.com>
2020-07-02 00:10:56 +0900

    Make it work

--- vue3-project-with-typescript/src/App.vue
+++ vue3-project-with-typescript/src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Hello Vue3</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import Vue, { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  components: {},
 });
 </script>
 
