6ca5e2d173a08295977bc585723b60fb8aa47289
bob1983 <bob.1983.g@gmail.com>
2020-07-05 23:41:55 +0900

    Added post store

--- vue3-project-with-typescript/src/store/index.ts
+++ vue3-project-with-typescript/src/store/index.ts
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import { users } from './modules/users'
+import { posts } from './modules/posts'
 
 export const store = createStore({
   modules: {
-    users
+    users,
+    posts
   }
 })
