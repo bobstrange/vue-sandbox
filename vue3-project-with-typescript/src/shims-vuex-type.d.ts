9d16038ceda154952278c84e817ad778f72115a2
bob1983 <bob.1983.g@gmail.com>
2020-07-08 00:25:45 +0900

    added type

--- vue3-project-with-typescript/src/shims-vuex-type.d.ts
+++ vue3-project-with-typescript/src/shims-vuex-type.d.ts
@@ -13,4 +13,24 @@ declare module 'vuex' {
   type Mutations<S, M> = {
     [K in keyof M]: (state: S, payload: M[K]) => void
   }
+
+  type ExCommit<M> = <T extends keyof M>(type: T, payload?: M[T]) => void
+  type ExDispatch<A> = <T extends keyof A>(
+    type: T,
+    payload?: A[T]
+  ) => Promise<any>
+  type ExActionContext<S, A, G, M> = {
+    dispatch: ExDispatch<A>
+    commit: ExCommit<M>
+    state: S
+    getters: G
+    rootState: RootState
+    rootGetters: RootGetters
+  }
+  type Actions<S, A, G = {}, M = {}> = {
+    [K in keyof A]: (
+      ctx: ExActionContext<S, A, G, M>,
+      payload: A[K]
+    ) => Promise<any>
+  }
 }
