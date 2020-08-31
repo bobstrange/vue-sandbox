463bfd0c86c5e0bac7d2488f9991f74faddff825
bobstrange <bob.1983.g@gmail.com>
2020-08-31 22:56:53 +0900

    intercept get and set

--- vue3-reactivity/src/02_proxy-and-reflect_01_intercept-get-and-set.js
+++ vue3-reactivity/src/02_proxy-and-reflect_01_intercept-get-and-set.js
@@ -37,5 +37,23 @@ proxiedProduct = new Proxy(product, {
   }
 })
 
-console.log(proxiedProduct.price)
+console.log(proxiedProduct.quantity)
+// 'Get was called with key = quantity'
+// 2
 
+proxiedProduct = new Proxy(product, {
+  get(target, key, receiver) {
+    console.log('Get was called with key = ' + key)
+    return Reflect.get(target, key, receiver)
+  },
+  set(target, key, value, receiver) {
+    console.log('Set was called with key = ' + key + ' and value = ' + value)
+    return Reflect.set(target, key, value, receiver)
+  }
+})
+
+proxiedProduct.quantity = 10
+// Set was called with key = quantity and value = 10
+console.log(proxiedProduct.quantity)
+// Get was called with key = quantity
+// 10
