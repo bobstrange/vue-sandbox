839b33215b074ac3d97e5cc5f4a26bf7446ec93f
bobstrange <bob.1983.g@gmail.com>
2020-09-05 23:09:39 +0900

    ref

---
+++ vue3-reactivity/src/03_active-effect-and-ref_02_ref.js
@@ -0,0 +1,96 @@
+const targetMap = new WeakMap()
+let activeEffect = null
+
+function track(target, key) {
+  if (activeEffect) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()))
+    }
+    let dep = depsMap.get(key)
+    if (!dep) {
+      depsMap.set(key, (dep = new Set()))
+    }
+    dep.add(activeEffect)
+  }
+}
+
+function trigger(target, key) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) {
+    return
+  }
+  const dep = depsMap.get(key)
+  if (dep) {
+    dep.forEach(effect => effect())
+  }
+}
+
+function reactive(target) {
+  const handler = {
+    get(target, key, receiver) {
+      const result = Reflect.get(target, key, receiver)
+      track(target, key)
+      return result
+    },
+    set(target, key, value, receiver) {
+      const oldValue = target[key]
+      const result = Reflect.set(target, key, value, receiver)
+      if (result && oldValue != value) {
+        trigger(target, key)
+      }
+      return result
+    }
+  }
+  return new Proxy(target, handler)
+}
+
+function ref(raw) {
+  const r = {
+    get value() {
+      track(r, 'value')
+      return raw
+    },
+    set value(newVal) {
+      raw = newVal
+      trigger(r, 'value')
+    },
+  }
+  return r
+}
+
+function effect(eff) {
+  activeEffect = eff
+  activeEffect()
+  activeEffect = null
+}
+
+let product = reactive({ price: 5, quantity: 2 })
+let salePrice = ref(0)
+let total = 0
+
+effect(() => {
+  salePrice.value = product.price * 0.8
+})
+
+effect(() => {
+  total = salePrice.value * product.quantity
+})
+
+console.log(
+  `Before updated total (should be 10) = ${total} salePrice (should be 4) = ${salePrice.value}`
+)
+
+product.quantity = 3
+
+console.log(
+  `After updated total (should be 15) = ${total} salePrice (should be 4) = ${salePrice.value}`
+)
+
+product.price = 100
+
+console.log(
+  `After updated total (should be 300) = ${total} salePrice (should be 80) = ${salePrice.value}`
+)
+
+
