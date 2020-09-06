4d68c11c0384d72b8ec46ee48770ceeb8f2353be
bobstrange <bob.1983.g@gmail.com>
2020-09-06 23:21:02 +0900

    Added vuex

--- vue-auth/src/App.vue
+++ vue-auth/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="">
+  <div>
     <TheNav />
-    <router-view class="page">
+    <router-view class="page" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import TheNav from './components/TheNav.vue';
+import { defineComponent } from "vue";
+import TheNav from "./components/TheNav.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    TheNav
+    TheNav,
   },
 });
 </script>
--- vue-auth/src/main.js
+++ vue-auth/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { routes } from "./router";
+import { store } from "./store";
 
 import "./index.css";
 
 const app = createApp(App);
 
 app.use(routes);
+app.use(store);
 app.mount("#app");
 
 export { app };
