39db75af892921343b31a71c1ff63caa284dc21b
bobstrange <bob.1983.g@gmail.com>
2020-09-22 23:13:23 +0900

    init

---
+++ vue-graphql/client/src/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p class="username">{{ currentUser.username }}'s posts:</p>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.content }}</li>
+    </ul>
+    <div>
+      <input v-model="newPostContent" />
+      <button @click="addPost()">Add Post</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: function() {
+    return {
+      currentUser: { username: "user" },
+      posts: [],
+      newPostContent: ""
+    };
+  },
+  methods: {
+    addPost() {
+      this.posts.push({ content: this.newPostContent });
+      this.newPostContent = "";
+    }
+  }
+};
+</script>
