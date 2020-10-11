495962ec51f7bb20b500a645ec68acf4f728c124
bobstrange <bob.1983.g@gmail.com>
2020-10-11 23:42:21 +0900

    toggle read

--- email/src/components/MailView.vue
+++ email/src/components/MailView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="email-display">
+    <div>
+      <button>Archive</button>
+      <button @click="toggleRead()">
+        {{ showEmail.read ? 'Mark Unread' : 'Mark Read' }}
+      </button>
+      <button>Newer</button>
+      <button>Older</button>
+    </div>
     <h2 class="mb-0">
       Subject: <strong>{{ showEmail.subject }}</strong>
     </h2>
@@ -18,20 +26,22 @@ import { computed, defineComponent, PropType } from 'vue'
 import { format } from 'date-fns'
 import marked from 'marked'
 import { Email } from '@/types/Email'
+import { updateEmail } from '@/apis/emailClient'
 
-type MailViewProps = {
-  email: Email | null
-}
 export default defineComponent({
-  setup(props: MailViewProps) {
+  setup(props) {
     const showEmail = computed<Partial<Email>>(() => {
-      return props.email || {}
+      return props.email
     })
-    return { showEmail, format, marked }
+    const toggleRead = async () => {
+      const email = props.email
+      console.log(await updateEmail({ ...email, read: !email.read }))
+    }
+    return { showEmail, format, marked, toggleRead }
   },
   props: {
     email: {
-      type: Object as PropType<Email | null>,
+      type: Object as PropType<Email>,
       required: true
     }
   }
