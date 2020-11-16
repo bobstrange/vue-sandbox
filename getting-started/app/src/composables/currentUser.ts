a954431e13e2d8f6356016fb122548c9943bcf5e
bobstrange <bob.1983.g@gmail.com>
2020-11-16 23:58:06 +0900

    lint

--- getting-started/app/src/composables/currentUser.ts
+++ getting-started/app/src/composables/currentUser.ts
@@ -1,20 +1,20 @@
-import { ref, InjectionKey, inject } from "vue"
-import { User } from "@/models/user"
+import { ref, InjectionKey, inject } from "vue";
+import { User } from "@/models/user";
 
 export const CurrentUserStoreKey: InjectionKey<CurrentUserStore> = Symbol(
   "CurrentUserStore"
-)
+);
 
 export const useCurrentUser = () => {
-  const currentUser = ref<User | null>(null)
+  const currentUser = ref<User | null>(null);
   const setCurrentUser = (user: User) => {
-    currentUser.value = user
-  }
-  return { currentUser, setCurrentUser }
-}
+    currentUser.value = user;
+  };
+  return { currentUser, setCurrentUser };
+};
 
-export type CurrentUserStore = ReturnType<typeof useCurrentUser>
+export type CurrentUserStore = ReturnType<typeof useCurrentUser>;
 
 export const injectCurrentUser = () => {
-  return inject(CurrentUserStoreKey)
-}
+  return inject(CurrentUserStoreKey);
+};
