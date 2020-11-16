04e07cc0facf29cd2f21e47ea9b50242aee93172
bobstrange <bob.1983.g@gmail.com>
2020-11-16 23:57:43 +0900

    Added usersStore

---
+++ getting-started/app/src/composables/users.ts
@@ -0,0 +1,23 @@
+import { ref, onBeforeMount, inject, InjectionKey } from "vue"
+import { fetchUsers } from "@/apis/userClient"
+import { User } from "@/models/user"
+
+export const UsersStoreKey: InjectionKey<UsersStore> = Symbol("UsersStore")
+
+export const useUsers = () => {
+  const users = ref<User[]>([])
+  const reloadUsers = async () => {
+    const data = await fetchUsers()
+    users.value = data
+  }
+
+  onBeforeMount(() => reloadUsers())
+
+  return { users, reloadUsers }
+}
+
+export type UsersStore = ReturnType<typeof useUsers>
+
+export const injectUsers = () => {
+  return inject(UsersStoreKey)
+}
