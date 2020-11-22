37fb0a8aa4ec32fee861dd04eb4781bf421ef8a2
bobstrange <bob.1983.g@gmail.com>
2020-11-22 16:56:43 +0900

    added mongoose

--- getting-started/server/src/config.ts
+++ getting-started/server/src/config.ts
@@ -1,3 +1,29 @@
 import 'dotenv/config'
 
 export const PORT = Number(process.env.APP_PORT || '8080')
+
+if (!process.env.DB_HOST) {
+  throw new Error('Please define DB_HOST')
+}
+
+if (!process.env.DB_PORT) {
+  throw new Error('Please define DB_PORT')
+}
+
+if (!process.env.DB_USER) {
+  throw new Error('Please define DB_USER')
+}
+
+if (!process.env.DB_PASSWORD) {
+  throw new Error('Please define DB_PASSWORD')
+}
+
+if (!process.env.DB_NAME) {
+  throw new Error('Please define DB_NAME')
+}
+
+export const DB_HOST = process.env.DB_HOST
+export const DB_PORT = process.env.DB_PORT
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_NAME = process.env.DB_NAME
--- getting-started/server/src/index.ts
+++ getting-started/server/src/index.ts
@@ -1,7 +1,21 @@
 import { app } from './app'
-import { PORT } from './config'
+import mongoose from 'mongoose'
+import { PORT, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from './config'
 
-const start = () => {
+const start = async () => {
+  try {
+    await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}`, {
+      db: DB_NAME,
+      user: DB_USER,
+      pass: DB_PASSWORD,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+  } catch (error) {
+    console.error(error)
+  }
+
   app.listen(PORT, () => {
     console.log(`Listening on localhost:${PORT}`)
   })
