ee023a9a44308e82bf9e24b22b17a7b313e47147
bobstrange <bob.1983.g@gmail.com>
2020-11-24 23:55:44 +0900

    added home description

--- getting-started/app/src/views/Home.vue
+++ getting-started/app/src/views/Home.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>Vue getting started</h1>
+    <p>Hi, this is vue getting started page. You can login, logout, see any posts without login, and create/edit post if you've logged in.</p>
+
+    <p v-if="!isLoggedIn">Have already account ? Please <router-link :to="{ name: 'Login' }">Login</router-link></p>
+
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { injectAuthStore } from '@/store/authStore'
+import { computed, defineComponent } from "vue"
 
 export default defineComponent({
   name: "Home",
+  setup() {
+    const { getters: { loggedIn }} = injectAuthStore()
+    const isLoggedIn = computed(() => loggedIn())
+    return {
+      isLoggedIn
+    }
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+
+a {
+  display: inline-block;
+  font-weight: bold;
+}
+</style>
