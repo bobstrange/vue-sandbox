d868013890a8839f13f595386df73042ee1f1b2c
bobstrange <bob.1983.g@gmail.com>
2020-12-30 20:50:52 +0900

    feat: move events data to page component and styling

--- real-world-vue3/src/components/EventCard.vue
+++ real-world-vue3/src/components/EventCard.vue
@@ -1,22 +1,29 @@
 <template>
   <div
-    class="p-6 cursor-pointer bg-white rounded border-solid border border-gray-900 max-w-xs shadow-sm transform hover:scale-105 hover:shadow-md transition duration-100"
+    class="p-6 cursor-pointer bg-white rounded border-solid border border-gray-900 w-56 shadow-sm transform hover:scale-105 hover:shadow-md transition duration-100"
   >
     <span class="text-gray-500 text-sm"
-      >{{ event.time }} on {{ event.date }}</span
+      >{{ data.time }} on {{ data.date }}</span
     >
-    <h4 class="text-gray-900 font-semibold">{{ event.title }}</h4>
+    <h4 class="text-gray-900 font-semibold">{{ data.title }}</h4>
   </div>
 </template>
 
 <script>
-import dummyEvents from '@/dummy_events.json'
-
+import { computed } from 'vue'
 export default {
   name: 'EventCard',
-  setup() {
-    const event = dummyEvents[0]
-    return { event }
+  setup(props) {
+    const data = computed(() => {
+      return props.event
+    })
+    return { data }
+  },
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
   }
 }
 </script>
--- real-world-vue3/src/views/Home.vue
+++ real-world-vue3/src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="home">
-    <EventCard />
+    <h1 class="text-gray-900 font-bold text-xl">Events For Good</h1>
+    <ul class="mt-4 flex flex-col items-center">
+      <li v-for="event in events" :key="event.id" class="first:mt-0 mt-4">
+        <EventCard :event="event" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import dummyEvents from '@/dummy_events.json'
 
 export default {
   name: 'Home',
+  setup() {
+    const events = dummyEvents
+    return { events }
+  },
   components: {
     EventCard
   }
