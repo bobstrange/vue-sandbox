e18a6147437ef43e3fa1cf7d366bc276832e6949
bobstrange <bob.1983.g@gmail.com>
2020-12-30 20:01:35 +0900

    feat: added event card

--- real-world-vue3/src/components/EventCard.vue
+++ real-world-vue3/src/components/EventCard.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <h3>Event Card</h3>
+  <div class="event-card">
+    <span>{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
   </div>
 </template>
 
 <script>
+import dummyEvents from '@/dummy_events.json'
+
 export default {
-  name: 'EventCard'
+  name: 'EventCard',
+  setup() {
+    const event = dummyEvents[0]
+    return { event }
+  }
 }
 </script>
 
