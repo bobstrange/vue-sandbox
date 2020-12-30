2ed740b9e49266d770b71f1eb5798eaaa2e45776
bobstrange <bob.1983.g@gmail.com>
2020-12-30 20:56:00 +0900

    fix: change Home.vue -> EventList.vue

--- real-world-vue3/src/views/EventList.vue
+++ real-world-vue3/src/views/EventList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="event-list">
     <h1 class="text-gray-900 font-bold text-xl">Events For Good</h1>
     <ul class="mt-4 flex flex-col items-center">
       <li v-for="event in events" :key="event.id" class="first:mt-0 mt-4">
@@ -14,7 +14,7 @@ import EventCard from '@/components/EventCard.vue'
 import dummyEvents from '@/dummy_events.json'
 
 export default {
-  name: 'Home',
+  name: 'EventList',
   setup() {
     const events = dummyEvents
     return { events }
