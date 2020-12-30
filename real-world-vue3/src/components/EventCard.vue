404de665cccd691e724ccfcc45662cc567678d91
bobstrange <bob.1983.g@gmail.com>
2020-12-30 20:31:18 +0900

    feat: add simple style to event card

--- real-world-vue3/src/components/EventCard.vue
+++ real-world-vue3/src/components/EventCard.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="event-card">
-    <span>{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
+  <div
+    class="p-6 cursor-pointer bg-white rounded border-solid border border-gray-900 max-w-xs shadow-sm transform hover:scale-105 hover:shadow-md transition duration-100"
+  >
+    <span class="text-gray-500 text-sm"
+      >{{ event.time }} on {{ event.date }}</span
+    >
+    <h4 class="text-gray-900 font-semibold">{{ event.title }}</h4>
   </div>
 </template>
 
