bb90bccb405b38682e08d753ae00f667ea24b005
bobstrange <bob.1983.g@gmail.com>
2021-03-05 23:35:42 +0900

    feat: install eslint and prettier then configure them

--- component-design-patterns/.eslintrc.js
+++ component-design-patterns/.eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-  root: true,
   env: {
-    node: true
+    browser: true,
+    es2021: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+}
