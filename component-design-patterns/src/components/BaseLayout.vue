b7b198a25da80e0fdaf4c31d39d18a129f8f3cf6
bobstrange <bob.1983.g@gmail.com>
2021-03-06 23:26:55 +0900

    feat: add layout

---
+++ component-design-patterns/src/components/BaseLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="base-layout">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot name="default"></slot>
+    </main>
+    <aside>
+      <slot name="sidebar"></slot>
+    </aside>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.base-layout {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  background: #f5f5f5;
+  width: 960px;
+  margin: auto;
+}
+
+header {
+  width: 100%;
+  background: #ccffff;
+}
+
+h1 {
+  margin: 0;
+}
+
+main {
+  width: 80%;
+  background: #ffccff;
+  min-height: 400px;
+}
+
+aside {
+  width: 20%;
+  background: #ffffcc;
+}
+
+footer {
+  width: 100%;
+  background: #afafaf;
+}
+</style>
--- component-design-patterns/src/views/About.vue
+++ component-design-patterns/src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <BaseLayout>
+    <template v-slot:header>
+      <h1>Page Title</h1>
+    </template>
+    <template v-slot:default>
+      <p>Page content</p>
+    </template>
+    <template v-slot:sidebar>
+      <ul>
+        <li>Link 01</li>
+        <li>Link 02</li>
+      </ul></template
+    >
+    <template v-slot:footer>
+      Footer
+    </template>
+  </BaseLayout>
 </template>
+
+<script setup lang="ts">
+import BaseLayout from '@/components/BaseLayout.vue'
+</script>
+
+<style scoped></style>
