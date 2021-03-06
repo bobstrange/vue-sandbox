bd3709c02ba2644b6a41ce726e65a8af69579281
bobstrange <bob.1983.g@gmail.com>
2021-03-06 23:08:16 +0900

    feat: add base button

---
+++ component-design-patterns/src/components/BaseButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="button">
+    <slot>Submit</slot>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- component-design-patterns/src/views/Home.vue
+++ component-design-patterns/src/views/Home.vue
@@ -5,9 +5,11 @@
       :watched="true"
       :image="require('@/assets/logo.png')"
     />
+    <BaseButton></BaseButton>
   </div>
 </template>
 
 <script setup lang="ts">
-import MoviePoster from '../components/MoviePoster.vue'
+import MoviePoster from '@/components/MoviePoster.vue'
+import BaseButton from '@/components/BaseButton.vue'
 </script>
