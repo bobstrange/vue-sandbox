c14435bf68b9bdc5b81f659f04a266312f7a2110
bobstrange <bob.1983.g@gmail.com>
2021-03-06 22:50:41 +0900

    feat: add props

---
+++ component-design-patterns/src/components/MoviePoster.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ title }}</h1>
+    <p>{{ length }} <span v-if="watched">✅</span></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps({
+  image: {
+    type: String,
+    default: '../assets/logo.png'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  length: {
+    type: [Number, String],
+    default: 90
+  },
+  watched: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<style scoped></style>
--- component-design-patterns/src/views/Home.vue
+++ component-design-patterns/src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MoviePoster title="Test Title" :watched="true" />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-});
+<script setup lang="ts">
+import MoviePoster from '../components/MoviePoster.vue'
 </script>
