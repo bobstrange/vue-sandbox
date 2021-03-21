5a0511db7c0d505daa7e6e6bc92d4755427006b3
bobstrange <bob.1983.g@gmail.com>
2021-03-21 23:04:53 +0900

    feat: add events

---
+++ real-world-vue3/src/components/EventCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link :to="{ name: 'Home' }" class="event-link">
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from '@vue/runtime-core'
+
+const props = defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
---
+++ real-world-vue3/src/views/EventList.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Events</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+
+const events = []
+</script>
