ac62cd23d00d7d9406a6895f971254901c9b85b5
bobstrange <bob.1983.g@gmail.com>
2021-04-10 23:51:56 +0900

    feat: write failing test

---
+++ unit-testing-vue3/src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive, toRefs } from '@vue/reactivity'
+
+const state = reactive({ loggedIn: false })
+
+const { loggedIn } = toRefs(state)
+</script>
