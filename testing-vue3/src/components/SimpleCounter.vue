bed26fac90445e6f7b63d27c9654a302a87d15bb
bobstrange <bob.1983.g@gmail.com>
2021-04-11 22:21:21 +0900

    feat: implemented simple counter

---
+++ testing-vue3/src/components/SimpleCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Times clicked: {{ count }}</p>
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "SimpleCounter",
+  setup() {
+    const count = ref(0);
+    const increment = () => {
+      count.value = count.value + 1;
+    };
+    return {
+      count,
+      increment,
+    };
+  },
+});
+</script>
--- testing-vue3/src/views/Home.vue
+++ testing-vue3/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <SimpleCounter />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import SimpleCounter from "@/components/SimpleCounter.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    SimpleCounter,
   },
 });
 </script>
