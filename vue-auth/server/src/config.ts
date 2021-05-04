02241746644cd46cf16da583e972e12f3f167fe5
bobstrange <bob.1983.g@gmail.com>
2021-05-04 23:30:15 +0900

    tidy

--- vue-auth/server/src/config.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-
-export const PORT = Number(process.env.PORT || "8080");
-const secretKey = process.env.SECRET_KEY;
-if (!secretKey) {
-  throw new Error("SECRET_KEY must be defined");
-}
-export const SECRET_KEY = secretKey;
--- vue-auth/server/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { app } from "./app";
-import { PORT } from "./config";
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
