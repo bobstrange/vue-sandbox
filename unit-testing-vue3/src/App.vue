fc229f1f47cecfa5e1bc851e4c76908eae297a87
bobstrange <bob.1983.g@gmail.com>
2021-05-08 23:56:27 +0900

    feat: add AppHeader

--- unit-testing-vue3/src/App.vue
+++ unit-testing-vue3/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app-container">
+    <AppHeader />
+    <router-view />
   </div>
-  <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppHeader from '@/components/AppHeader.vue'
+
+export default defineComponent({
+  components: { AppHeader },
+})
+</script>
 
 <style>
 #app {
@@ -14,17 +22,8 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.app-container {
+  max-width: 1000px;
+  margin: 0 auto;
 }
 </style>
---
+++ unit-testing-vue3/src/components/AppHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+    <button v-show="loggedIn">Logout</button>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      loggedIn: true,
+    })
+    return toRefs(state)
+  },
+})
+</script>
+
+<style scoped>
+nav {
+  padding: 10px;
+  background-color: #42b983;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+}
+
+li a {
+  margin-right: 10px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #222;
+}
+
+button {
+  height: 30px;
+  padding: 5px;
+  border-radius: 5px;
+  border-style: none;
+  background-color: white;
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: 20px;
+}
+</style>
