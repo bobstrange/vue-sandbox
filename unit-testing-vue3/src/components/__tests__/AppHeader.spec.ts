40fc8786fca37be0b78c8102feb0ceb395783931
bobstrange <bob.1983.g@gmail.com>
2021-05-09 22:20:48 +0900

    feat: add logout button test

--- unit-testing-vue3/src/components/__tests__/AppHeader.spec.ts
+++ unit-testing-vue3/src/components/__tests__/AppHeader.spec.ts
@@ -7,8 +7,17 @@ describe('AppHeader', () => {
     expect(wrapper.find('button').isVisible()).toBeFalsy()
   })
 
-  test('If user is logged in, show logout button', async () => {
+  test('If user is logged in, show logout button', () => {
     const wrapper = shallowMount(AppHeader, { props: { loggedIn: true } })
     expect(wrapper.find('button').isVisible()).toBeTruthy()
+  })
+
+  test('If user clicks logout, logout has been called', async () => {
+    const logoutMock = jest.fn()
+    const wrapper = shallowMount(AppHeader, {
+      props: { loggedIn: true, logout: logoutMock },
+    })
+    await wrapper.find('button').trigger('click')
+    expect(logoutMock).toHaveBeenCalled()
   })
 })
