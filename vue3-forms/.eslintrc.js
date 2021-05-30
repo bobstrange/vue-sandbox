6859348884db8b5b99035940152989f132a484ce
bobstrange <bob.1983.g@gmail.com>
2021-05-30 21:01:28 +0900

    feat: add cypress eslint settings

--- vue3-forms/.eslintrc.js
+++ vue3-forms/.eslintrc.js
@@ -3,17 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    'cypress/globals': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/essential',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint'],
+  plugins: ['vue', '@typescript-eslint', 'cypress'],
   rules: {},
 }
