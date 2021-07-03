5e7a4c70519d55c5733871827cfea3ee012b636d
bobstrange <bob.1983.g@gmail.com>
2021-07-03 21:27:06 +0900

    tmp

--- vue3-forms/cypress/integration/example.ts
+++
@@ -1,8 +0,0 @@
-describe('Example', () => {
-  it('has dropdown', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Create an event')
-    cy.get('select').select('nature')
-    cy.get('input')
-  })
-})
