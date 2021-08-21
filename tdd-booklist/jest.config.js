c4afe683325424d4af3e1a7993a22e982bcea8ac
bobstrange <bob.1983.g@gmail.com>
2021-08-21 22:46:00 +0900

    feat: fix config for testing

--- tdd-booklist/jest.config.js
+++ tdd-booklist/jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.vue$': 'vue-jest',
+  },
 }
