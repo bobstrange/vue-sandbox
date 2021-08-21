dd96669c5e25d6671759b0e46fbfd9585f5840a7
bobstrange <bob.1983.g@gmail.com>
2021-08-22 00:03:22 +0900

    feat: show posts

--- tdd-vue/src/App.vue
+++ tdd-vue/src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <h1 class="text-2xl">Book List</h1>
+  <h1 class="text-2xl">Post List</h1>
+  <ul v-for="post in posts" :key="post.id">
+    <li>{{ post.title }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+import { client } from './lib/api/client'
+import { Post } from './types'
 
 export default defineComponent({
   name: 'App',
+  setup() {
+    const posts = reactive<Post[]>([])
+    client
+      .get<Post[]>('/posts')
+      .then((res) => {
+        posts.push(...res.data)
+      })
+      .catch((e) => console.error(e))
+    return { posts }
+  },
 })
 </script>
 
