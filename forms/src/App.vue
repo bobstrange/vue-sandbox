b352ced8a82da1b84b38d8c2da9b06d6b8d544a4
bobstrange <bob.1983.g@gmail.com>
2021-08-28 23:45:46 +0900

    feat: add create event form

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1>Top</h1>
+  <div>
+    <h1>Top</h1>
+    <CreateEvent />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import CreateEvent from './components/CreateEvent.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    CreateEvent,
+  },
 })
 </script>
 
@@ -15,8 +22,9 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1000px;
+  margin: 0 auto;
+  background-color: aliceblue;
 }
 </style>
