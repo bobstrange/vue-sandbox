a558f3fbc9c77c52ee3d4093f0ff18142a8eb4a5
bobstrange <bob.1983.g@gmail.com>
2021-08-31 22:30:41 +0900

    feat: add PostDetail acceptance test

--- tdd-vue/cypress/integration/app.spec.ts
+++ tdd-vue/cypress/integration/app.spec.ts
@@ -8,4 +8,13 @@ describe('app', () => {
     cy.visit('/')
     cy.get('.post-item').should('have.length', 100)
   })
+
+  it('goes to the detail page', () => {
+    cy.visit('/')
+    cy.get('.post-item > a').first().click()
+    cy.url().should('include', '/posts/1')
+    cy.get('.post-title').contains(
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+    )
+  })
 })
