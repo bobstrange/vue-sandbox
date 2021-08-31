5cb9b476d8ce6275d57bc179e6df8ea483cab1e4
bobstrange <bob.1983.g@gmail.com>
2021-08-31 22:04:00 +0900

    refactor: make router.ts

--- tdd-vue/src/main.ts
+++ tdd-vue/src/main.ts
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import './index.css'
-import PostList from './views/PostList.vue'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [{ path: '/', component: PostList }],
-})
+import { router } from './router'
 
 createApp(App).use(router).mount('#app')
