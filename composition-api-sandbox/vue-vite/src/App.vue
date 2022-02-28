84ce132bf1ac8d30c137b537bd1ff40732d3ba69
bobstrange <bob.1983.g@gmail.com>
2022-02-28 23:27:30 +0900

    tmp

--- composition-api-sandbox/vue-vite/src/App.vue
+++ composition-api-sandbox/vue-vite/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import MyInput from './components/MyInput.vue'
+
+const myValue = 'Hoge'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <MyInput label="Test" v-model="myValue" />
+  <div>binded value: {{ myValue }}</div>
 </template>
 
 <style>
---
+++ composition-api-sandbox/vue-vite/src/components/MyInput.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+  modelValue: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+</script>
+
+<template>
+  <div class="group">
+    <label>{{ label }}</label>
+    <input
+      type="text"
+      :value="props.modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
