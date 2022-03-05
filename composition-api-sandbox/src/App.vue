a9b96ab8d2c6c18169f6c80784279ffb48ce571e
bobstrange <bob.1983.g@gmail.com>
2022-03-05 21:10:52 +0900

    feat: two-way bindings for text

--- composition-api-sandbox/src/App.vue
+++ composition-api-sandbox/src/App.vue
@@ -2,33 +2,40 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <UserList :users="users" /> -->
-    <CompositionUserList :users="users" @deleteUser="userDeleteClicked" />
+    <!-- <CompositionUserList :users="users" @deleteUser="userDeleteClicked" /> -->
+    <MyText v-model="MyTextValue" />
+    <p>Input value: {{ MyTextValue }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed } from '@vue/composition-api'
 // import UserList from './components/UserList.vue'
-import CompositionUserList from '@/components/CompositionUserList.vue'
-import { User } from '@/model/User'
+// import CompositionUserList from '@/components/CompositionUserList.vue'
+import MyText from '@/components/MyText.vue'
+// import { User } from '@/model/User'
 
 export default defineComponent({
   name: 'App',
   components: {
     // UserList,
-    CompositionUserList
+    // CompositionUserList
+    MyText
   },
   setup(props, { root }) {
-    const users = computed<User[]>(() => {
-      return root.$store.getters['user/getUsers']
-    })
-    function userDeleteClicked(id: number) {
-      root.$store.dispatch('user/deleteUser', id)
-    }
-    return {
-      users,
-      userDeleteClicked
-    }
+    // const users = computed<User[]>(() => {
+    //   return root.$store.getters['user/getUsers']
+    // })
+    // function userDeleteClicked(id: number) {
+    //   root.$store.dispatch('user/deleteUser', id)
+    // }
+    // return {
+    //   users,
+    //   userDeleteClicked
+    // }
+
+    const MyTextValue = ref('default')
+    return { MyTextValue }
   }
 })
 </script>
---
+++ composition-api-sandbox/src/components/MyText.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-checkbox">
+    <h1>MyText</h1>
+    <input type="text" v-model="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-checkbox',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  watch: {
+    value(newValue) {
+      this.$emit('input', newValue)
+    }
+  }
+}
+</script>
+
+<style></style>
