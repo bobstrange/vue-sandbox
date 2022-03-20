d719f37a9b6ff12e4ec8a49b67308fece7cbb632
bobstrange <bob.1983.g@gmail.com>
2022-03-20 10:43:00 +0900

    feat: creating input

---
+++ vue3-form/src/components/BaseInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <label>{label}</label>
+  <input :placeholder="label" class="field" />
+</template>
+
+<script setup lang="ts">
+const { label = '' } = defineProps<{ label?: string }>()
+</script>
+
+<style scoped></style>
