5407b7a9c186345c8eedcd741fc2a74eb78eb968
bobstrange <bob.1983.g@gmail.com>
2022-03-22 00:03:12 +0900

    feat: submit

--- vue3-form/src/components/SimpleForm.vue
+++ vue3-form/src/components/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
       <BaseSelect
         :options="categories"
         v-model="event.category"
@@ -30,12 +30,13 @@
 </template>
 
 <script setup lang="ts">
+import axios from 'axios'
 import { reactive } from '@vue/reactivity'
 import BaseInput from './BaseInput.vue'
 import BaseSelect from './BaseSelect.vue'
 import BaseCheckbox from './BaseCheckbox.vue'
-import BaseRadio from './BaseRadio.vue'
 import BaseRadioGroup from './BaseRadioGroup.vue'
+
 const categories = [
   'sustainability',
   'nature',
@@ -61,4 +62,16 @@ const petOptions = [
   { label: 'Yes', value: 1 },
   { label: 'No', value: 0 },
 ]
+
+const sendForm = async () => {
+  try {
+    const res = await axios.post(
+      'https://my-json-server.typicode.com/bobstrange/vue-sandbox/events',
+      event
+    )
+    console.log('Response', res)
+  } catch (e) {
+    console.error('Error', e)
+  }
+}
 </script>
